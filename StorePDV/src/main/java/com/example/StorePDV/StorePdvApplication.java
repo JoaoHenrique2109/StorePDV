17065fe877d60941f4035a75ad39415398febebb
João Silva <jhenriqueb2109@gmail.com>
2026-05-08 00:54:38 -0300

    Add files via upload

---
+++ StorePDV/src/main/java/com/example/StorePDV/StorePdvApplication.java
@@ -0,0 +1,13 @@
+package com.example.StorePDV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorePdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorePdvApplication.class, args);
+	}
+
+}
